7d67c63b913cbb7c3fe455bbdcd93777b8f5a8bf
Vitor Santos <vitor93gs@gmail.com>
2024-07-31 16:13:32 -0300

    first commit

---
+++ src/constants.js
@@ -0,0 +1,37 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+  plant:
+    "A lush green plant. They say talking to plants helps them grow. Maybe it's listening to your next big idea!",
+
+  degree:
+    "Framed and proudly displayed, my degree in Electrical Engineering is a testament to years of hard work and late-night study sessions. Alongside it are certificates from bootcamps in Data Science and Web Development. I'm currently pursuing a postgrad course in AI, and soon, that certificate will join the collection.",
+
+  bookcase:
+    "Books upon books, a world of adventures and knowledge. Among the coding classics like 'Clean Code,' you might also find books in Portuguese, French, and a bit of Spanish and German. Always something new to learn!",
+
+  bed: "Ah, the bed. A place of rest, dreams, and where some of the best game ideas are born. Who says productivity and sleep can't mix?",
+
+  sofa: "This cozy sofa has seen countless movie marathons and brainstorming sessions. Perfect for those 'Eureka!' moments.",
+
+  Lola: "Lola, my border collie, is always ready for a game of fetch. Just look at those eager eyes! Who's a good dog? You are, Lola!",
+
+  Lohana:
+    "Lohana, my wonderful wife and my rock. She's the player two in my life, always supporting my quests and adventures.",
+
+  Nino: "Nino, the other half of my dynamic duo of border collies. He's always up for a game and keeps the energy high around here!",
+
+  pc: 'My trusty PC, the command center for all my creative endeavors. Currently, it\'s where I delve into the worlds of TypeScript, React, Next.js, Nest.js, Python, and AI research. It\'s my gateway to innovation and creativity. <a href="https://github.com/vitor93gs" target="_blank">Check out my GitHub?</a>',
+
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a> Contact me at jslegend@protonmail.com if you have any interesting job opportunities! You can also find me on <a href="https://www.linkedin.com/in/vitor93gds/" target="_blank">LinkedIn</a>!'`,
+
+  cabinet:
+    "A cabinet filled with secrets, or just snacks. You'll never know until you open it! But beware, you might find my secret stash of ideas.",
+
+  fridge:
+    "The fridge: home to leftovers and midnight snacks. Fuel for those long coding sessions. What's inside? Only the brave dare to find out.",
+
+  table:
+    "This table is where culinary magic happens. Preparing food here is just another creative outlet. Maybe you'll find some recipes alongside the utensils.",
+
+  exit: "Ready to step out into the real world? Adventure awaits beyond this door. But don't forget to save your progress before you go!",
+};
