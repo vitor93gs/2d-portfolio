841c8393bc48073d179bbe2773dd730acb9e06f3
Vitor Santos <vitor93gs@gmail.com>
2024-07-31 16:33:31 -0300

    fixed tiping and links

--- src/constants.js
+++ src/constants.js
@@ -4,10 +4,10 @@ export const dialogueData = {
     "A lush green plant. They say talking to plants helps them grow. Maybe it's listening to your next big idea!",
 
   degree:
-    "Framed and proudly displayed, my degree in Electrical Engineering is a testament to years of hard work and late-night study sessions. Alongside it are certificates from bootcamps in Data Science and Web Development. I'm currently pursuing a postgrad course in AI, and soon, that certificate will join the collection.",
+    "Framed and proudly displayed, my degree in Electrical Engineering is a testament to years of hard work and late-night study sessions. I will eventually hang beside it my certificates from bootcamps in Data Science and Web Development. I'm currently pursuing a postgrad course in AI, and soon, that certificate will join the collection.",
 
   bookcase:
-    "Books upon books, a world of adventures and knowledge. Among the coding classics like 'Clean Code,' you might also find books in Portuguese, French, and a bit of Spanish and German. Always something new to learn!",
+    "Books upon books, a world of adventures and knowledge. Among the coding classics like 'Clean Code,' you might also find books in Portuguese, French, and a bit of Spanish and German (some of the languages I speak). Always something new to learn!",
 
   bed: "Ah, the bed. A place of rest, dreams, and where some of the best game ideas are born. Who says productivity and sleep can't mix?",
 
@@ -20,9 +20,9 @@ export const dialogueData = {
 
   Nino: "Nino, the other half of my dynamic duo of border collies. He's always up for a game and keeps the energy high around here!",
 
-  pc: 'My trusty PC, the command center for all my creative endeavors. Currently, it\'s where I delve into the worlds of TypeScript, React, Next.js, Nest.js, Python, and AI research. It\'s my gateway to innovation and creativity. <a href="https://github.com/vitor93gs" target="_blank">Check out my GitHub?</a>',
+  pc: `My trusty PC, the command center for all my creative endeavors. Currently, it\'s where I delve into the worlds of TypeScript, React, Next.js, Nest.js, Python, and AI research. It\'s my gateway to innovation and creativity. If you are also a technology enthusiast why don't you <a href="https://github.com/vitor93gs" target="_blank">Check out my GitHub</a>!`,
 
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/vitor93gs/2d-portfolio/blob/main/public/Vitor_Santos_CV.pdf" target="_blank">Check it out?</a> Contact me at vitor93gs@gmail.com if you have any interesting job opportunities! You can also find me on <a href="https://www.linkedin.com/in/vitor93gds/" target="_blank">LinkedIn</a>!'`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/vitor93gs/2d-portfolio/blob/main/public/Vitor_Santos_CV.pdf" target="_blank">Check it out?</a> Contact me at vitor93gs@gmail.com if you have any interesting job opportunities! You can also find me on <a href="https://www.linkedin.com/in/vitor93gds/" target="_blank">LinkedIn</a>!`,
 
   cabinet:
     "A cabinet filled with secrets, or just snacks. You'll never know until you open it! But beware, you might find my secret stash of ideas.",
